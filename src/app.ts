42a97c95d753d808e92f8d9141e04c7fc60b5792
pantho0 <panthomashreky6@gmail.com>
2025-06-29 13:05:09 +0600

    user controller added with error handler middlewares and functions

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/v1', router);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('UMSHOP SERVER IS RUNNING');
 });
+  
+app.use(globalErrorHandler)
 
 export default app;
